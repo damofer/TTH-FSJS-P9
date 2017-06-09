2ac65ce048e0e7f912b05e974e7520bf5d8ec212
David Mogollon <dmogollon@uninorte.edu.co>
2017-06-09 16:50:17 -0500

    edit and delete recipes

--- public/scripts/controllers/RecipesController.js
+++ public/scripts/controllers/RecipesController.js
@@ -1,21 +1,53 @@
 app.controller('RecipesController',function($scope,dataService){
+ 	$scope.recipes=[];
 
 
- dataService.getAllCategories(function(response){
- 	$scope.categories = response.data;
- 	
- });
- dataService.getAllRecipes(function(response){
- 	$scope.recipes = response.data;
- 	console.log($scope.recipes)
- });
-
- //This piece of code allows you to get all the recipes from a selected category.
-	var select = document.getElementsByTagName('select')[0];
-	select.addEventListener('change',function(evt){
-		
-		dataService.getRecipesByCategory(evt.target.value,function(response){
+$scope.getAllCategories = function(){
+	 dataService.getAllCategories(function(response){
+	 	$scope.categories = response.data;
+	 	
+	 });
+ }
+$scope.getAllRecipes = function(){
+	 dataService.getAllRecipes(function(response){
+	 	$scope.recipes = response.data;
+	 	console.log($scope.recipes)
+	 });
+}
+$scope.getRecipesByCategory = function(category){
+		dataService.getRecipesByCategory(category,function(response){
 			$scope.recipes = response.data;
 		});
+}
+ //This piece of code allows you to get all the recipes from a selected category.
+	var select = document.getElementsByTagName('select')[0];
+
+
+	select.addEventListener('change',function(evt){
+		$scope.getRecipesByCategory(evt.target.value);
+		
 	});
+
+
+
+	$scope.deleteRecipe = function(recipe_id){
+		dataService.deleteRecipe(recipe_id);
+		if(select.value != ''){
+			$scope.getRecipesByCategory(select.value);
+		}else{
+			$scope.getAllRecipes();
+		}
+	}
+	$scope.addRecipe = function(recipe_id){
+		dataService.deleteRecipe(recipe_id);
+		if(select.value != ''){
+			$scope.getRecipesByCategory(select.value);
+		}else{
+			$scope.getAllRecipes();
+		}
+	}
+
+
+ 	$scope.getAllRecipes();
+ 	$scope.getAllCategories();
 });
