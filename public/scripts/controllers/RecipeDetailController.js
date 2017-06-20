878bb35e80aa546558eff3ac5446c1319a4c6d28
David Mogollon <dmogollon@uninorte.edu.co>
2017-06-20 10:28:54 -0500

    added all the basic functionalities of the details page

--- public/scripts/controllers/RecipeDetailController.js
+++ public/scripts/controllers/RecipeDetailController.js
@@ -3,6 +3,8 @@ app.controller('RecipeDetailController',function($scope,dataService,$location,$w
 $scope.categories = {};
 $scope.recipe ={};
 $scope.ingredients={};
+$scope.foodItems = {};
+
 $scope.getAllCategories = function(){
 	 dataService.getAllCategories(function(response){
 	 	$scope.categories = response.data;
@@ -19,6 +21,45 @@ $scope.getRecipe = function (id) {
 		});
  }
  	
+ $scope.getFoodItems = function(){
+ 	dataService.getFoodItems(function(response){
+			$scope.foodItems = response.data;
+			console.log($scope.foodItems);
+			
+			
+		});
+ }
+ 
+ $scope.deleteRecipe = function(id){
+ 	dataService.getFoodItems(function(response){
+			$scope.foodItems = response.data;
+			console.log($scope.foodItems);
+			
+			
+		});
+ }
+
+ $scope.delete= function(item,list) { 
+  var index = list.indexOf(item);
+  list.splice(index, 1);     
+}
+$scope.addIngredient=function(){
+	var ingredient = {
+		"amount":"",
+		"condition":"",
+		"foodItem":"Select a food Item"
+	};
+	 $scope.ingredients.push(ingredient);
+	
+}
+$scope.addStep = function(){
+	var step = {
+		"description":""
+	};
+	 $scope.recipe.steps.push(step);
+}
+
  	$scope.getAllCategories();
  	$scope.getRecipe($routeParams.id);
+ 	 $scope.getFoodItems();
 });
